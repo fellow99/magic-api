01e592398c7b66eb27ab2ceda5d6dcb21831fd76
fellow99 <fellow99@163.com>
2026-05-03 23:15:33 +0800

    feat: 添加runner运行器子工程

---
+++ runner/src/main/java/com/magicapi/runner/MagicApiRunner.java
@@ -0,0 +1,11 @@
+package com.magicapi.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicApiRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(MagicApiRunner.class, args);
+    }
+}
